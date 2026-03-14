086c2cd550a72aea9271341245315845ca2d817f
MyNameISBocchi <espinaalfonso123@gmail.com>
2026-03-14 15:23:41 -0400

    metodos tablas intermedias

--- app/Services/PersonService.php
+++ app/Services/PersonService.php
@@ -1,12 +1,18 @@
 <?php
 namespace App\Services;
 use App\Models\Person;
+use App\Models\PersonComunity;
+use App\Models\PersonCouncil;
 use App\Models\PersonRole;
 use Illuminate\Support\Facades\Crypt;
 
 class PersonService{
     public function store(array $person){
         $arrRoles = json_decode($person['roleId']);
+        $arrCommittee = json_decode($person['committeeId']);
+         $arrComunities = json_decode($person['comunityId']);
+         $arrCouncils = json_decode($person['councilId']);
+         
         $duplicate = Person::select('id')->where([['identification',$person['identification']],['email',$person['email']],['phone',$person['phone']],])->first();
         if($duplicate){
             return false;
@@ -14,18 +20,53 @@ class PersonService{
             $countryDecrypted = Crypt::decrypt($person['cityId']);
             $person['cityId'] = $countryDecrypted;
             $person = Person::create($person);
-
+            
             PersonRole::where('personId', $person->id)->delete();
-            $arrInsert = [];
+            $rolesInsert = [];
 
             for($i = 0; $i < count($arrRoles); $i++){
                 $roleIdDecrypted = Crypt::decrypt($arrRoles[$i]);
-                $arrInsert[] =[
+                $rolesInsert[] =[
                     'personId' => $person->id,
-                    'roleId' => $roleIdDecrypted
+                    'roleId' => $roleIdDecrypted,
+                    'created_at' => now(),
+                    'updated_at' => now()
                 ];
             }
-            PersonRole::insert($arrInsert);
+            if(!empty($rolesInsert)){
+                PersonRole::insert($rolesInsert);
+            }
+            
+            PersonComunity::where('personId', '=', $person->id)->delete();
+            $insertComunities = [];
+            for($i = 0; $i < count($arrComunities); $i++){
+                $ComunityDecrypted = Crypt::decrypt($arrComunities[$i]);
+                $insertComunities[] = [
+                    'personId' => $person->id,
+                    'comunityId' => $ComunityDecrypted,
+                    'created_at' => now(),
+                    'updated_at' => now()
+                ];
+            }
+            if(!empty($insertComunities)){
+                PersonComunity::insert($insertComunities);
+            }
+            PersonCouncil::where('personId', '=', $person->id)->delete();
+            $insertCouncils = [];
+            for($i = 0;  $i < count($arrCouncils); $i++){
+                $councilDecrypted = Crypt::decrypt($arrCouncils[$i]);
+                $insertCouncils[] = [
+                    'personId' => $person->id,
+                    'councilId' => $councilDecrypted,
+                    'created_at' => now(),
+                    'updated_at' => now()
+                ];
+            }
+            if(!empty($insertCouncils)){
+                PersonCouncil::insert($insertCouncils);
+            }
+
+            
             return true;
         }
 
